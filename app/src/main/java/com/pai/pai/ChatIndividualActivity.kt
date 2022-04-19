a392a5855406f944b2380f9eb4c8a459d9e132b1
PeterGarza12 <pedro.garzagnz@uanl.edu.mx>
2022-04-18 21:07:04 -0500

    a
    a

--- app/src/main/java/com/pai/pai/ChatIndividualActivity.kt
+++ app/src/main/java/com/pai/pai/ChatIndividualActivity.kt
@@ -23,7 +23,12 @@ class ChatIndividualActivity : AppCompatActivity() {
     private val chatAdaptador = AdaptadorChat(listMessages)
 
     private val database = FirebaseDatabase.getInstance()
+<<<<<<< Updated upstream
     private var chatRef = database.getReference("")
+=======
+    private lateinit var chatRef: DatabaseReference
+    private lateinit var msgRef: DatabaseReference
+>>>>>>> Stashed changes
     private lateinit var nombreUsuario: String
     private lateinit var idUsuarioDestino: String
 
@@ -39,20 +44,32 @@ class ChatIndividualActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chat_individual)
 
         idUsuarioDestino = intent.getStringExtra("idUsuario") ?: "sin id"
+<<<<<<< Updated upstream
         //Toast.makeText(this, idUsuarioDestino, Toast.LENGTH_SHORT).show()
+=======
+
+>>>>>>> Stashed changes
 
 
 
         nombreUsuario = intent.getStringExtra("username") ?: "sin nombre"
 
+<<<<<<< Updated upstream
         chatRef = database.getReference("chats/chatsIndividuales") //crea la rama o tabla de chats.
+=======
+        //chatRef = database.getReference("chats/chatsIndividuales") //crea la rama o tabla de chats.
+>>>>>>> Stashed changes
         val rvMensajes = findViewById<RecyclerView>(R.id.rv_Messages)
         val btnEnviar = findViewById<Button>(R.id.btnEnviar_chat)
         val txtMensaje = findViewById<EditText>(R.id.txtMensaje_chat)
         val btnReturn = findViewById<ImageView>(R.id.btnRegresar_chatInd)
         rvMensajes.adapter = chatAdaptador
 
+<<<<<<< Updated upstream
         createOrFindChat(Chat("", idUsuarioDestino, user!!.uid), idUsuarioDestino)
+=======
+        chatRef = createOrFindChat(Chat("", idUsuarioDestino, user!!.uid), idUsuarioDestino)
+>>>>>>> Stashed changes
 
 
         btnEnviar.setOnClickListener {
@@ -70,11 +87,19 @@ class ChatIndividualActivity : AppCompatActivity() {
         }
     }
 
+<<<<<<< Updated upstream
     private fun createOrFindChat(chat: Chat, idUsuarioDestino: String){
 
 
 
         chatRef.addValueEventListener(object: ValueEventListener {
+=======
+    private fun createOrFindChat(chat: Chat, idUsuarioDestino: String): DatabaseReference{
+
+        msgRef = database.getReference("chats/chatsIndividuales") //crea la rama o tabla de chats.
+
+        msgRef.addValueEventListener(object: ValueEventListener {
+>>>>>>> Stashed changes
 
             var flag = false
 
@@ -85,7 +110,11 @@ class ChatIndividualActivity : AppCompatActivity() {
                     val chat2: Chat = snap.getValue(Chat::class.java) as Chat
 
                     if((idUsuarioDestino == chat2.user1 || idUsuarioDestino == chat2.user2) && (user!!.uid == chat2.user1 || user!!.uid == chat2.user2)){
+<<<<<<< Updated upstream
                         chatRef = database.getReference("chats/chatsIndividuales/"+chat2.id)
+=======
+                        msgRef = database.getReference("chats/chatsIndividuales/"+chat2.id).child("Mensajes")
+>>>>>>> Stashed changes
                         Toast.makeText(this@ChatIndividualActivity, "Sí existe el chat", Toast.LENGTH_SHORT).show()
                         flag = true
                         break
@@ -98,6 +127,10 @@ class ChatIndividualActivity : AppCompatActivity() {
                     chat.id = firebaseMsg.key ?: ""
                     Toast.makeText(this@ChatIndividualActivity, "Ya se creó el chat", Toast.LENGTH_SHORT).show()
                     firebaseMsg.setValue(chat)
+<<<<<<< Updated upstream
+=======
+                    msgRef = database.getReference("chats/chatsIndividuales/"+chat.id+"/Mensajes")
+>>>>>>> Stashed changes
                 }
 
             }
@@ -107,9 +140,18 @@ class ChatIndividualActivity : AppCompatActivity() {
                 Toast.makeText(this@ChatIndividualActivity, "Error al leer mensajes", Toast.LENGTH_SHORT).show()
             }
         })
+<<<<<<< Updated upstream
     }
 
     private fun sendMessage(message: Message){
+=======
+
+        return msgRef
+    }
+
+    private fun sendMessage(message: Message){
+        chatRef = msgRef
+>>>>>>> Stashed changes
         val firebaseMsg = chatRef.push()
         message.id = firebaseMsg.key ?: ""
 
@@ -121,6 +163,10 @@ class ChatIndividualActivity : AppCompatActivity() {
         val rvMensajes = findViewById<RecyclerView>(R.id.rv_Messages)
         rvMensajes.adapter = chatAdaptador
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         chatRef.addValueEventListener(object: ValueEventListener {
 
             override fun onDataChange(snapshot: DataSnapshot) {
